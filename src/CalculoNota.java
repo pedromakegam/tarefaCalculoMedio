551c6dcf885ced06cc7ca489c85abf9fc24be38c
pedrovanderlansantana <pedromakegam@gmail.com>
2024-04-30 23:55:34 -0300

    exercicio modulo

--- src/CalculoNota.java
+++ src/CalculoNota.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class CalculoNota {
+public class CalculoNota {
+
+    private int nota1;
+
+    private int nota2;
+
+    private int nota3;
+
+    private int nota4;
+
+
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
+
+    public void setNota3(int nota3) {
+        this.nota3 = nota3;
+    }
+
+    public void setNota4(int nota4) {
+        this.nota4 = nota4;
+    }
+
+    public double calculoMediaNota(){
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+        return  media;
+
+   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+
+    CalculoNota mediaDaNota = new CalculoNota();
+
+    mediaDaNota.setNota1(7);
+    mediaDaNota.setNota2(4);
+    mediaDaNota.setNota3(6);
+    mediaDaNota.setNota4(5);
+
+    System.out.println(mediaDaNota.calculoMediaNota());
 }
